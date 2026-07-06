0af37b8cd4f882556b482a8e1e66c8e0e2bceacf
Hemant Dahit <hemantdahit123456@gmail.com>
2026-07-06 18:56:35 +0545

    added something

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Use a relative base so the built app can be hosted from GitHub Pages
-  // or any subpath / custom domain without absolute asset paths breaking.
-  base: './',
-})
+  base: "/kegle_exercise/",
+});
